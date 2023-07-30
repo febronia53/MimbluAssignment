95b2239d2815c65297fd4585f7a990eac927d302
febronia53 <febroniasameh5@gmail.com>
2023-07-30 23:30:23 +0300

    add design for app

---
+++ app/src/main/java/com/example/mimbluassignment/SymptomsFragment.kt
@@ -0,0 +1,20 @@
+package com.example.mimbluassignment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class SymptomsFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_symptoms, container, false)
+    }
+
+}
