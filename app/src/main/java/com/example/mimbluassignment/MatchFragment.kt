31691a1b2b657408b45db1e863b4905287cfbe0c
febronia53 <febroniasameh5@gmail.com>
2023-07-31 04:03:34 +0300

    add nave controller

--- app/src/main/java/com/example/mimbluassignment/MatchFragment.kt
+++ app/src/main/java/com/example/mimbluassignment/MatchFragment.kt
@@ -5,16 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.mimbluassignment.databinding.FragmentMatchBinding
 
 class MatchFragment : Fragment() {
 
+    private var _binding: FragmentMatchBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_match, container, false)
+        _binding = FragmentMatchBinding.inflate(inflater, container, false)
+        backToPreviousFragment()
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+    private fun backToPreviousFragment(){
+        binding.iconBack.setOnClickListener { v->
+            v.findNavController().popBackStack()
+        }
+    }
 
 }
--- app/src/main/java/com/example/mimbluassignment/SymptomsFragment.kt
+++ app/src/main/java/com/example/mimbluassignment/SymptomsFragment.kt
@@ -5,16 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.mimbluassignment.databinding.FragmentSymptomsBinding
 
 
 class SymptomsFragment : Fragment() {
 
 
+    private var _binding: FragmentSymptomsBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_symptoms, container, false)
+        _binding = FragmentSymptomsBinding.inflate(inflater, container, false)
+        goToMatchFragment()
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun goToMatchFragment() {
+        binding.buttonShowMatches.setOnClickListener { v ->
+            v.findNavController().navigate(R.id.action_symptomsFragment_to_matchFragment)
+        }
+    }
 }
